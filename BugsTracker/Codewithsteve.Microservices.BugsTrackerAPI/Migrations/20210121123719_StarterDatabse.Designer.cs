490d01910f3101be78f4a6ac0ef5ffe3a5f0126d
Stephen Ndiananie <stevendi23@yahoo.com>
2021-01-21 13:04:44 +0000

    Implement adding starter data to migration

--- BugsTracker/Codewithsteve.Microservices.BugsTrackerAPI/Migrations/20210121123719_StarterDatabse.Designer.cs
+++ BugsTracker/Codewithsteve.Microservices.BugsTrackerAPI/Migrations/20210121123719_StarterDatabse.Designer.cs
@@ -4,14 +4,16 @@ using Codewithsteve.Microservices.BugsTracker.API;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Codewithsteve.Microservices.BugsTrackerAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210121123719_StarterDatabse")]
+    partial class StarterDatabse
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
